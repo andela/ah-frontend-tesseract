d3a5cb90b00890b5ce4a818a4365bae9d6ba2f67
cmplx-xyttmt <archimedesisaac@gmail.com>
2018-10-02 09:36:59 +0300

    chore(redux): setup redux
    - add redux and react-redux to dependencies
    - create actions, containers, reducers folders
    
    [Finishes #160814313]

---
+++ src/containers/readme.txt
@@ -0,0 +1,2 @@
+This folder is for the container components;
+for each container component, create a new .js file.
--- src/index.js
+++ src/index.js
@@ -4,14 +4,21 @@ import {BrowserRouter as Router} from 'react-router-dom'
 import './components/styles/index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './reducers';
+
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
-
-    (<Router>
-
-        <App />
-
-    </Router>
-    ), document.getElementById('root'));
+    <Provider store={store}>
+        <Router>
+            <App/>
+        </Router>
+    </Provider>
+    ,
+    document.getElementById('root')
+);
 
 registerServiceWorker();
